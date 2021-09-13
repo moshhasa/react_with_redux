147806f8476869954ef874b1d8d903b5da5c5841
Ernest <ernestduodu@hotmail.co.uk>
2021-09-13 12:11:57 +0100

    Replaced redux with  reduxjs toolkit/ with multiple slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,50 +1,12 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
-
- const initialState = {
-    counter : 0,
-    showCounter : true
-}
-
-
-const counterSlice = createSlice({
-    name : 'counter',
-    initialState : initialState,
-    reducers : {
-        increment(state, action) {
-            state.counter = state.counter + action.payload
-        },
-        decrement(state) {
-            state.counter--;
-        },
-        toggleCounterDisplay(state) {
-            state.showCounter = !state.showCounter;
-        }
-    }
-})
-
-
-
-// const reducer =  (state = initialState, { type, value }) => {
-   
-//     switch (type) {
-//     case "increment":  
-//         return { ...state, counter : state.counter+ value }
-//     case "decrement":
-//             return { ...state, counter : state.counter-1}
-//     case "toggleCounterDisplay" :
-//         return {...state, showCounter : !state.showCounter}
-//     default:
-//         return state
-//     }
-// }
-
-//const store = createStore(reducer);
-
+import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from "./auth-slice";
+import counterSliceReducer from "./counter-slice";
 
 const store = configureStore({
-    reducer :  counterSlice.reducer
-    
+  reducer: {
+    counter: counterSliceReducer,
+    auth: authSliceReducer,
+  },
 });
 
-export const counterActions = counterSlice.actions;
 export default store;
